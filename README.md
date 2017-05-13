eab03ff1b4bddce2499e573130ecc20247a64495
Johnson Lai <superoo7@Johnsons-MacBook-Pro.local>
2017-05-13 20:14:20 +0800

    initialised with Xcode project and README.md

---
+++ README.md
@@ -0,0 +1,23 @@
+# iOT light bulb
+* A lightbulb that are able to control using iOS application or web
+* current stage planning to make localhost first
+
+## Materials
+* Light bulb (4.5V)
+* nodeMCU
+* USB Cable
+
+## Tools
+* Version control: Git
+* Text editor:
+	* NodeMCU: Arduino IDE
+	* iOS App: Xcode
+	* website: Atom / Sublime Text
+	* iOT platform: ThingSpeak (if not localhost)
+
+## LICENSE
+MIT License. Refer to license.txt
+
+## Contributors
+[superoo7](http://superoo7.com)
+
---
+++ iOT-light-bulb/iOT-light-bulb/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iOT-light-bulb
+//
+//  Created by Johnson Lai on 13/05/2017.
+//  Copyright © 2017 johnsonlwh7. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
