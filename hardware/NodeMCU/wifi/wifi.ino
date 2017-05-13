4ab6a9ce7b831edf6736e0b4a42e1107fe8ff5b8
Johnson Lai <superoo7@Johnsons-MacBook-Pro.local>
2017-05-13 20:23:34 +0800

    added reference and NodeMCU file

---
+++ hardware/NodeMCU/wifi/wifi.ino
@@ -0,0 +1,111 @@
+#include "ESP8266WiFi.h"
+
+// WiFI WPA2
+const char* ssid = "";      // ssid
+const char* password = "";  // password
+
+int ledPin = 13; // GPIO13
+WiFiServer server(80);
+
+void wifiSetup();
+void startServer();
+
+void setup() {
+  Serial.begin(115200);
+  wifiSetup();
+}
+
+void loop() {
+  startServer();
+}
+
+
+void wifiSetup() {
+  delay(10);
+
+  pinMode(ledPin, OUTPUT);
+  digitalWrite(ledPin, LOW);
+
+  // Connect to WiFi network
+  Serial.println();
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("WiFi connected");
+
+  // Start the server
+  server.begin();
+  Serial.println("Server started");
+
+  // Print the IP address
+  Serial.print("Use this URL to connect: ");
+  Serial.print("http://");
+  Serial.print(WiFi.localIP());
+  Serial.println("/");
+}
+
+void startServer() {
+  // Check if a client has connected
+  WiFiClient client = server.available();
+  if (!client) {
+    return;
+  }
+
+  // Wait until the client sends some data
+  Serial.println("new client");
+  while(!client.available()){
+    delay(1);
+  }
+
+  // Read the first line of the request
+  String request = client.readStringUntil('\r');
+  Serial.println(request);
+  client.flush();
+
+  // Match the request
+
+  int value = LOW;
+  if (request.indexOf("/LED=ON") != -1)  {
+    digitalWrite(ledPin, HIGH);
+    value = HIGH;
+  }
+  if (request.indexOf("/LED=OFF") != -1)  {
+    digitalWrite(ledPin, LOW);
+    value = LOW;
+  }
+
+// Set ledPin according to the request
+//digitalWrite(ledPin, value);
+
+  // Return the response
+  client.println("HTTP/1.1 200 OK");
+  client.println("Content-Type: text/html");
+  client.println(""); //  do not forget this one
+  client.println("<!DOCTYPE HTML>");
+  client.println("<html><head><title>On/Off LED</title><link rel=\"stylesheet\" href=\"https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css\" integrity=\"sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u\" crossorigin=\"anonymous\"><script src=\"https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js\" integrity=\"sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa\" crossorigin=\"anonymous\"></script></head><div class=\"container row col-md-12 col-xs-12\"><h1 class=\"text-center\">");
+
+  client.print("Led pin is now: ");
+
+  if(value == HIGH) {
+    client.print("On");
+  } else {
+    client.print("Off");
+  }
+  client.println("</h1></div class=\"text-center col-md-12 col-xs-12\"><br><br>");
+  client.println("<a class=\"text-center\" href=\"/LED=ON\"\"><button type=\"button\" class=\"btn btn-lg btn-success\">Turn On </button></a>");
+  client.println("<a class=\"text-center\" href=\"/LED=OFF\"\"><button type=\"button\" class=\"btn btn-lg btn-danger\">Turn Off </button></a><br />");
+  client.println("</html>");
+
+  delay(1);
+  Serial.println("Client disonnected");
+  Serial.println("");
+}
+
---
+++ reference/NodeMCU/Lua/script1.lua
@@ -0,0 +1,24 @@
+-- Config
+local pin = 13            --> GPIO2
+local value = gpio.LOW
+local duration = 1000    --> 1 second
+
+
+-- Function toggles LED state
+function toggleLED ()
+    if value == gpio.LOW then
+        value = gpio.HIGH
+    else
+        value = gpio.LOW
+    end
+
+    gpio.write(pin, value)
+end
+
+
+-- Initialise the pin
+gpio.mode(pin, gpio.OUTPUT)
+gpio.write(pin, value)
+
+-- Create an interval
+tmr.alarm(0, duration, 1, toggleLED)
